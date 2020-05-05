2b34b2800072d5e0533e7529f95f79cd25ef0abc
kwu3 <kwu3@villanova.edu>
2020-05-05 15:11:50 -0400

    Created schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NOT NULL
+);
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL
+
+);
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+name VARCHAR(30)
+);
